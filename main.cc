1829c320c24cee41e77ddfa2694a1fd28896d9de
flaviooozzz <00115625@uca.edu.sv>
2025-08-28 15:34:02 -0600

    :sparkles: Continuacion 2

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@ int main()
     cout<<"grupo 4 \n";
     cout<<"Diego Armando Mata Cortez \n";
     cout<<"Cesar Andres Olmedo Lobos \n";
+    cout<<"Flavio Cesar Pineda Caceres \n";
     
     return 0;
 }
